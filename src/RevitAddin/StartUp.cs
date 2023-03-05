6a23fbc7ea50724292b70a7d4b46919fcdddd213
陈杰 <jiegou04@gmail.com>
2023-03-05 09:56:10 +0800

    packages改用 nuget

--- src/RevitAddin/StartUp.cs
+++ src/RevitAddin/StartUp.cs
@@ -5,21 +5,19 @@ using RevitAddin.UI.Windows;
 
 namespace RevitAddin
 {
-	/// <summary>
-	/// Class StartUp.
-	/// </summary>
-	public class StartUp
-	{
-
-		/// <summary>
-		/// Configures the IoC service registerations.
-		/// </summary>
-		public void ConfigureServices()
-		{
-			IoC.Default
-			  .RegisterSingleton<IMainWindow, MainWindow>()
-			  .RegisterSingleton<IMainWindowViewModel, MainWindowViewModel>()
-			  ;
-		}
-	}
+    /// <summary>
+    /// Class StartUp.
+    /// </summary>
+    public class StartUp
+    {
+        /// <summary>
+        /// Configures the IoC service registerations.
+        /// </summary>
+        public void ConfigureServices()
+        {
+            IoC.Default
+              .RegisterSingleton<IMainWindow, MainWindow>()
+              .RegisterSingleton<IMainWindowViewModel, MainWindowViewModel>();
+        }
+    }
 }
